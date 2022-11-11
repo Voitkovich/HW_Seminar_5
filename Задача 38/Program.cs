23cfc528911675cd7481d95298dc3241cd786b3e
Ekaterina Voitkovich <styardess@mail.ru>
2022-11-11 15:14:07 +0300

    Ex 38

--- Задача 38/Program.cs
+++ Задача 38/Program.cs
@@ -2,3 +2,22 @@
 // Найдите разницу между максимальным и минимальным элементов массива.
 
 // [3 7 22 2 78] -> 76
+
+int[] array = new int [10];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random(). Next(1, 10);
+    Console.Write(array[i] + " , ");
+}
+int min = array[0];
+int max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max) max = array[i];
+    if (array[i] < min) min = array[i];
+    i++;
+}
+
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива равна: " 
+    + (max - min));
